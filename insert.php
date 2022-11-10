63d7c97a8f2682cfade02db18909b356a464556f
Inżynier Domu <inzynier.domu@gmail.com>
2022-11-10 22:50:00 +0100

    add debug

--- insert.php
+++ insert.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (isset($_POST['submit'])) {
     if (isset($_POST['code']) && isset($_POST['name'])) 
 	{    
